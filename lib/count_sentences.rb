53b94d6bce54f257b35b4d8c8d0c1ed50924f318
zhifeng ou <zhifengou@hotmail.com>
2018-09-26 13:45:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,27 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")
-      true 
-    else 
-      false 
-    end 
+    # if self.end_with?(".")
+    #   true 
+    # else 
+    #   false 
+    # end 
+    self.end_with?(".")?true:false
   end
 
   def question?
-  if self.end_with?("?")
-    true 
-  else 
-    false 
-  end
+  # if self.end_with?("?")
+  #   true 
+  # else 
+  #   false 
+  # end
+  self.end_with?("?")?true:false
 end 
 
   def exclamation?
-  if self.end_with?("!")
-      true 
-    else 
-      false 
-    end 
+  # if self.end_with?("!")
+  #     true 
+  #   else 
+  #     false 
+  #   end 
+  self.end_with?("!")?true:false
   end
 
   def count_sentences
