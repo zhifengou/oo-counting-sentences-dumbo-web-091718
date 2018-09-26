6c85583af78cef9d0e3b83dc7bc08463cbad6e0b
zhifeng ou <zhifengou@hotmail.com>
2018-09-26 13:42:12 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -21,11 +21,11 @@ class String
 end 
 
   def exclamation?
-  if self.end_with?("!")
-      true 
-    else 
-      false 
-    end 
+  # if self.end_with?("!")
+  #     true 
+  #   else 
+  #     false 
+  #   end 
   end
 
   def count_sentences
