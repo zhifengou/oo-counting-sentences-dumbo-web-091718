bbf98850c80acba8dfe59ff444e6d353166be8f3
zhifeng ou <zhifengou@hotmail.com>
2018-09-26 13:38:57 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,11 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with?(".")
-      true 
-    else 
-      false 
-    end 
+    # if self.end_with?(".")
+    #   true 
+    # else 
+    #   false 
+    # end 
   end
 
   def question?
